d908df5cc0dd9e7d16cbf58c7910b715bee5af74
ColinvD <22950@ma-web.nl>
2019-06-20 22:01:38 +0200

    win lose visual

---
+++ Assets/Scripts/CanvasManager/CanvasHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasHandler : MonoBehaviour
+{
+    [SerializeField]
+    private string _winText;
+    [SerializeField]
+    private string _loseText;
+    [SerializeField]
+    private Text _text;
+    [SerializeField]
+    private Canvas _Endscreen;
+
+    public void ChangeText(bool winContition) {
+        _text.text = (winContition) ? _winText : _loseText;
+    }
+
+    public void SetCanvasActive() {
+        _Endscreen.enabled = !_Endscreen.enabled;
+    }
+}
