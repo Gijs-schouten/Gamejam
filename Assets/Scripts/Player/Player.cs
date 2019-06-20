5870bbc70817bb24e1ec500e57dd3763210728a3
Dave Bakker <48511611+davebakkerdev@users.noreply.github.com>
2019-06-20 17:37:14 +0100

    Added player health

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int _health;
+    private int _currentHealth;
+    private int _damagePerHealth;
+
+    private void ReduceHealth(int health, int currentHealth, int damage)
+    {
+        health = currentHealth - damage;
+    }
+}
