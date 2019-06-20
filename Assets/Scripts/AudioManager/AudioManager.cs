4afe879b601b79da46f2cb24b21b3684543190b4
Tommiee <tomdepom1999@gmail.com>
2019-06-20 15:53:31 +0200

    Audiomanager logica to independent script

--- Assets/Scripts/AudioManager/AudioManager.cs
+++ Assets/Scripts/AudioManager/AudioManager.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour {
 
     public AudioClip[] _clips;
-    private AudioSource _source;
+    public AudioSource _source;
 
     void Awake() {
         _source = gameObject.GetComponent<AudioSource>();
     }
 
-    void PlayAudioClip(int _enemyID) {
+    public void PlayAudioClip(int _enemyID) {
         _source.clip = _clips[_enemyID];
         _source.Play();
     }
