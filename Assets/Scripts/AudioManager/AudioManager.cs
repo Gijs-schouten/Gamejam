3a040e4eec6b563ec3bd3a5d491d4936d2ca0de9
Tommiee <tomdepom1999@gmail.com>
2019-06-20 15:23:08 +0200

    Audiolistener + BeginSequence in Checker

---
+++ Assets/Scripts/AudioManager/AudioManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public AudioClip[] _clips;
+    private AudioSource _source;
+
+    void Awake() {
+        _source = gameObject.GetComponent<AudioSource>();
+    }
+
+    void PlayAudioClip(int _enemyID) {
+        _source.clip = _clips[_enemyID];
+        _source.Play();
+    }
+}
--- Assets/Scripts/BeginSequence/BeginSequenceGenerator.cs
+++ Assets/Scripts/BeginSequence/BeginSequenceGenerator.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class BeginSequenceGenerator : MonoBehaviour
 {
-    private int[] _beginSequenceInts;
+    private List<int> _beginSequenceInts;
     public int _beginSequenceLength;
     public int _NumberOfEnemyTypes;
 
-    // Start is called before the first frame update
+    public Checker _checker;
+
     void Start()
     {
         _beginSequenceInts = generateRandomInts(_beginSequenceLength);
-        foreach (int i in _beginSequenceInts){
-            print(i);
-        }
+        _checker.FillMySequence(_beginSequenceInts);
+        //play beginsequenceints audio
     }
 
-    private int[] generateRandomInts(int length) {
-        int[] _intArr = new int[length];
+    private List<int> generateRandomInts(int length) {
+        List<int> _intArr = new List<int>(length);
         for(int i =0; i < length; i++) {
             _intArr[i] = (int)Random.Range(0f, _NumberOfEnemyTypes);
         }
