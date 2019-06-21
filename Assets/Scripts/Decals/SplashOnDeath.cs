354dc4e9c6003ccf730ba4340d7edfe958399351
Dave Bakker <48511611+davebakkerdev@users.noreply.github.com>
2019-06-21 19:56:44 +0100

    Particle system in main

---
+++ Assets/Scripts/Decals/SplashOnDeath.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashOnDeath : MonoBehaviour
+{
+    public ParticleSystem _splashParticles;
+    public GameObject _splashPrefab;
+    public Transform _splashHolder;
+
+    public void SplashDeath()
+    {
+        GameObject splash = Instantiate(_splashPrefab, this.transform.position, Quaternion.identity) as GameObject;
+        splash.transform.SetParent(_splashHolder, true);
+        Splash splashScript = splash.GetComponent<Splash>();
+
+        _splashParticles.transform.position = this.transform.position;
+        _splashParticles.Play();
+    }
+}
