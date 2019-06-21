fcaf0bcc0908316b2b85f0e62da00e31cdf61d02
Gijs Schouten <21969@ma-web.nl>
2019-06-21 13:06:19 +0200

    tutorial highlighting

--- Assets/Scripts/BeginSequence/BeginSequenceTutorial.cs
+++ Assets/Scripts/BeginSequence/BeginSequenceTutorial.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class BeginSequenceTutorial : MonoBehaviour {
@@ -11,46 +13,59 @@ public class BeginSequenceTutorial : MonoBehaviour {
 	private BeginSequenceGenerator _generator;
 	[SerializeField]
 	private GameObject[] _enemySlots;
+	[SerializeField]
+	private float _time = 1;
 
 	private void Start() {
-		HighlightAll();
+		//HighlightAll();
 	}
 
-	private void HighlightAll() {
-		print(_generator._beginSequenceInts.Count);
+
+	public IEnumerator HighlightAll() {
+		print(_generator._beginSequenceInts.Count + "abc");
 		for (int i = 0; i < _generator._beginSequenceInts.Count; i++) {
 			switch (_generator._beginSequenceInts[i]) {
 				case -1:
+					yield return new WaitForSeconds(_time);
 					break;
 
 				case 0:
 					HighlightEnemy(_enemySlots[0].transform);
 					print(0);
+					yield return new WaitForSeconds(_time);
 					break;
 
 				case 1:
 					print(1);
 					HighlightEnemy(_enemySlots[1].transform);
+					yield return new WaitForSeconds(_time);
 					break;
 
 				case 2:
 					print(2);
 					HighlightEnemy(_enemySlots[2].transform);
+					yield return new WaitForSeconds(_time);
 					break;
 
 				case 3:
 					print(3);
 					HighlightEnemy(_enemySlots[3].transform);
+					yield return new WaitForSeconds(_time);
 					break;
 			}
 		}
 	}
 
 	private void HighlightEnemy(Transform transform) {
-		GameObject clone;
-		clone = Instantiate(_highlight);
+		print("clone");
+		GameObject clone = Instantiate(_highlight, transform, false);
 		clone.transform.position = transform.position;
-		Thread.Sleep(500);
-		Destroy(clone);
+		UILifeTime lifetime = UILifeTime.CreateComponent(clone, _time);
+		
+
+
+		//clone.transform.localScale += new Vector3(100, 100);
+		//Thread.Sleep(5000);
+		//Destroy(clone);
 	}
 }
