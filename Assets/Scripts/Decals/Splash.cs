f5fa7bd07c52575cc1509da1941b98bdb59d7fe2
Dave Bakker <48511611+davebakkerdev@users.noreply.github.com>
2019-06-21 14:22:00 +0100

    Added decals and particle system

---
+++ Assets/Scripts/Decals/Splash.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Splash : MonoBehaviour
+{
+    public enum SplashLocation
+    {
+        Ground,
+        Background,
+    }
+
+    public Color _backgroundSplashAlpha;
+    public float _minimumSize;
+    public float _maximumSize;
+
+    public Sprite[] _splashSprites;
+
+    private SplashLocation _splashLocation;
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void Initialize(SplashLocation splashLocation)
+    {
+        this._splashLocation = splashLocation;
+
+        SetSprite();
+        SetSize();
+        SetRotation();
+        SetLocationProperties();
+    }
+
+    private void SetSprite()
+    {
+        int randomIndex = Random.Range(0, _splashSprites.Length);
+        _spriteRenderer.sprite = _splashSprites[randomIndex];
+    }
+
+    private void SetSize()
+    {
+        float randomSize = Random.Range(_minimumSize, _maximumSize);
+        transform.localScale *= randomSize;
+    }
+
+    private void SetRotation()
+    {
+        float randomRotation = Random.Range(-360.0f, 360.0f);
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, randomRotation);
+    }
+
+    private void SetLocationProperties()
+    {
+        switch(_splashLocation)
+        {
+            case SplashLocation.Background:
+                _spriteRenderer.color = _backgroundSplashAlpha;
+                _spriteRenderer.sortingOrder = 0;
+                break;
+
+            case SplashLocation.Ground:
+                _spriteRenderer.maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
+                _spriteRenderer.sortingOrder = 3;
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Decals/SplashParticles.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashParticles : MonoBehaviour
+{
+    public ParticleSystem _splashParticles;
+    public GameObject _splashPrefab;
+    public Transform _splashHolder;
+    private List<ParticleCollisionEvent> _collisionEvents = new List<ParticleCollisionEvent>();
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ParticlePhysicsExtensions.GetCollisionEvents(_splashParticles, other, _collisionEvents);
+
+        int count = _collisionEvents.Count;
+
+        for (int i = 0; i < count; i++)
+        {
+            GameObject splash = Instantiate(_splashPrefab, _collisionEvents[i].intersection, Quaternion.identity) as GameObject;
+            splash.transform.SetParent(_splashHolder, true);
+            Splash splashScript = splash.GetComponent<Splash>();
+            splashScript.Initialize(Splash.SplashLocation.Ground);
+        }
+    }
+}
